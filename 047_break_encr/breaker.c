a804bbb26c5d88cbe84b161d5dc25a5d82e894b6
QianHuang <qh38@duke.edu>
2018-09-25 21:21:21 -0400

    -

---
+++ 047_break_encr/breaker.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int count_maxfreq(FILE * f) {
+  int arr[26] = {0};
+  int c;
+  int idx = 0;
+  while ((c = fgetc(f)) != EOF) {
+    idx = c - 'a';
+    ++arr[idx];
+  }
+  int maxIdx = 0;
+  int maxTemp = 0;
+  for (int i = 0; i < 26; ++i) {
+    if (arr[i] > maxTemp) {
+      maxIdx = i;
+      maxTemp = arr[i];
+    }
+  }
+  return (maxIdx - 4) % 26;
+}
+
+int main(int argc, char ** argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: breaker inputFileName\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("Could not open file");
+    return EXIT_FAILURE;
+  }
+  int key = count_maxfreq(f);
+  printf("%d\n", key);
+
+  if (fclose(f) != 0) {
+    perror("Failed to close the input file!");
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}

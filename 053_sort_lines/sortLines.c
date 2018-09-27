360d77ad454718e4e2c3a83657323ea3c82a5763
QianHuang <qh38@duke.edu>
2018-09-27 17:28:30 -0400

    -

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 //This function is used to figure out the ordering
 //of the strings in qsort.  You do not need
 //to modify it.
@@ -16,9 +15,64 @@ void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
 
+char ** Load_file(size_t * plen, int argc, char ** argv) {
+  char ** line = NULL;
+  if (argc == 1) {
+    char * curr = NULL;
+    size_t sz;
+    while ((getline(&curr, &sz, stdin) >= 0)) {
+      line = realloc(line, (*plen + 1) * sizeof(*line));
+      line[*plen] = curr;
+      curr = NULL;
+      ++(*plen);
+    }
+    free(curr);
+  }
+  else {
+    for (int i = 1; i < argc; ++i) {
+      FILE * f = fopen(argv[i], "r");
+      if (f == NULL) {
+        perror("Could not open file.");
+        exit(EXIT_FAILURE);
+      }
+      char * curr = NULL;
+      size_t sz;
+      while ((getline(&curr, &sz, f) >= 0)) {
+        line = realloc(line, (*plen + 1) * sizeof(*line));
+        line[*plen] = curr;
+        curr = NULL;
+        ++(*plen);
+      }
+      free(curr);
+
+      if (fclose(f) != 0) {
+        perror("Fail to close file.");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+  return line;
+}
+
 int main(int argc, char ** argv) {
-  
-  //WRITE YOUR CODE HERE!
-  
+  char ** line = NULL;
+  size_t length = 0;
+  if (argc >= 1) {
+    line = Load_file(&length, argc, argv);
+  }
+  else {
+    perror("Invalid input.");
+    return EXIT_FAILURE;
+  }
+  if (line == NULL) {
+    perror("Invalid.");
+    return EXIT_FAILURE;
+  }
+  sortData(line, length);
+  for (size_t i = 0; i < length; ++i) {
+    printf("%s", line[i]);
+  }
+
+  free(line);
   return EXIT_SUCCESS;
 }

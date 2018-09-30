e13700aec2b47331eaacac07a7c34515cb5f5958
QianHuang <qh38@duke.edu>
2018-09-29 21:03:22 -0400

    -

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -1,8 +1,16 @@
+#include "outname.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "outname.h"
 
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  char * outName = NULL;
+  outName = calloc(strlen(inputName) + 8, sizeof(*outName));
+  if (outName == NULL) {
+    exit(EXIT_FAILURE);
+  }
+  strcpy(outName, inputName);
+  strcat(outName, ".counts");
+  return outName;
 }

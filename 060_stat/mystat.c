a283a6a9a5d38a15aeac9331d7aabd8725bccd38
QianHuang <qh38@duke.edu>
2018-10-10 11:25:46 -0400

    -

--- 060_stat/mystat.c
+++ 060_stat/mystat.c
@@ -48,6 +48,44 @@ void idFileType(struct stat sb, char * s) {
   }
 }
 
+void idFileReadable(struct stat sb, char * s) {
+  switch (sb.st_mode & S_IFMT) {
+    case S_IFBLK:
+      s[0] = 'b';
+      break;
+    case S_IFCHR:
+      s[0] = 'c';
+      break;
+    case S_IFDIR:
+      s[0] = 'd';
+      break;
+    case S_IFIFO:
+      s[0] = 'p';
+      break;
+    case S_IFLNK:
+      s[0] = 'l';
+      break;
+    case S_IFREG:
+      s[0] = '-';
+      break;
+    case S_IFSOCK:
+      s[0] = 's';
+      break;
+    default:
+      break;
+  }
+  s[1] = ((sb.st_mode & S_IRUSR) == 0) ? '-' : 'r';
+  s[2] = ((sb.st_mode & S_IWUSR) == 0) ? '-' : 'w';
+  s[3] = ((sb.st_mode & S_IXUSR) == 0) ? '-' : 'x';
+  s[4] = ((sb.st_mode & S_IRGRP) == 0) ? '-' : 'r';
+  s[5] = ((sb.st_mode & S_IWGRP) == 0) ? '-' : 'w';
+  s[6] = ((sb.st_mode & S_IXGRP) == 0) ? '-' : 'x';
+  s[7] = ((sb.st_mode & S_IROTH) == 0) ? '-' : 'r';
+  s[8] = ((sb.st_mode & S_IWOTH) == 0) ? '-' : 'w';
+  s[9] = ((sb.st_mode & S_IXOTH) == 0) ? '-' : 'x';
+  s[10] = '\0';
+}
+
 int main(int argc, char ** argv) {
   struct stat sb;
   if (argc != 2) {
@@ -67,11 +105,14 @@ int main(int argc, char ** argv) {
          sb.st_blocks,
          sb.st_blksize,
          s);
-  free(s);
 
   printf("Device: %lxh/%lud\tInode: %-10lu  Links: %lu\n",
          (long)major(sb.st_dev),
          (long)minor(sb.st_dev),
          sb.st_ino,
          sb.st_nlink);
+  s = realloc(s, 11 * sizeof(*s));
+  idFileReadable(sb, s);
+  printf("Access: (%04o/%s)\n", sb.st_mode & ~S_IFMT, s);
+  free(s);
 }

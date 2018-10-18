4494dd80755de7b501fb3c91bf7a940ff35ed0cf
QianHuang <qh38@duke.edu>
2018-10-17 22:53:44 -0400

    -

--- 077_sort_cpp/sortLines.cpp
+++ 077_sort_cpp/sortLines.cpp
@@ -1,27 +1,42 @@
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+
 using std::string;
 
-int main() {
-  std::vector<string> v;
-  string s;
-  bool flag = false;
-  while (std::getline(std::cin, s)) {
-    v.push_back(s);
-    if (!flag) {
-      flag = true;
+int main(int argc, char ** argv) {
+  if (argc < 1) {
+    std::cerr << "ERROR." << std::endl;
+  }
+  if (argc == 1) {
+    std::vector<string> v;
+    string s;
+    while (std::getline(std::cin, s)) {
+      v.push_back(s);
+    }
+
+    std::sort(v.begin(), v.end());
+    for (std::vector<string>::const_iterator it = v.begin(); it != v.end(); ++it) {
+      std::cout << *it << std::endl;
     }
   }
-  if (!flag) {
-    std::cerr << "ERROR!" << std::endl;
+  else {
+    for (int i = 1; i < argc; ++i) {
+      std::vector<string> v;
+      string s;
+      std::ifstream inFile;
+      inFile.open(argv[i]);
+      while (std::getline(inFile, s)) {
+        v.push_back(s);
+      }
+      std::sort(v.begin(), v.end());
+      for (std::vector<string>::const_iterator it = v.begin(); it != v.end(); ++it) {
+        std::cout << *it << std::endl;
+      }
+      inFile.close();
+    }
   }
-
-  std::sort(v.begin(), v.end());
-  for (std::vector<string>::const_iterator it = v.begin(); it != v.end(); ++it) {
-    std::cout << *it << std::endl;
-  }
-
   return EXIT_SUCCESS;
 }

61c3de9aff9edfb54682e8b57fed33e6e6fad828
QianHuang <qh38@duke.edu>
2018-11-10 21:21:29 -0500

    -

--- 101_huff_tree/buildTree.cpp
+++ 101_huff_tree/buildTree.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
-  //WRITE ME!
-}  
+Node * buildTree(uint64_t * counts) {
+  priority_queue_t pq;
+  for (int i = 0; i < 257; ++i) {
+    if (counts[i] != 0) {
+      pq.push(new Node(i, counts[i]));
+    }
+  }
+  while (pq.size() > 1) {
+    Node * a = pq.top();
+    pq.pop();
+    Node * b = pq.top();
+    pq.pop();
+    Node * tmproot = new Node(a, b);
+    pq.push(tmproot);
+  }
+  Node * root = pq.top();
+  pq.pop();
+  return root;
+}
